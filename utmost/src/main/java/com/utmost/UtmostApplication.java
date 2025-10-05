18d473a442b10e841eebf0978504ea3c5c92ca94
eunjuni1221 <kokomo1221@naver.com>
2025-10-05 22:08:31 +0900

    new 프로젝트

---
+++ utmost/src/main/java/com/utmost/UtmostApplication.java
@@ -0,0 +1,13 @@
+package com.utmost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtmostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtmostApplication.class, args);
+	}
+
+}
---
+++ utmost/src/test/java/com/utmost/UtmostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utmost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtmostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
